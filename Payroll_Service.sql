8e4426d37460484d53a529bbe3cddfa215bc35a2
Aj007ar <aj007.ar@gmail.com>
2022-09-27 12:42:43 +0530

    [Ajay] Added: Employee Payroll SQL query for insert data into Table

--- Payroll_Service.sql
+++ Payroll_Service.sql
@@ -8,3 +8,7 @@ EmployeeName varchar(255),
 EmployeeSalary float,
 StartDate Date
 )
+-----UC-3 Insert Values to table-----
+Insert into employee_payroll
+(EmployeeName,EmployeeSalary,StartDate)
+values('Ajay Rahod','750000','2020-10-07'),('Vijay','650000','2019-05-08'),('Priya','450500','2021-01-17'),('Asif','950500','2017-12-12');

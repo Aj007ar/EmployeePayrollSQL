ae2dd2342989e1424fdff222606a4e2edb523018
Aj007ar <aj007.ar@gmail.com>
2022-09-27 12:38:06 +0530

    [Ajay] Added: Employee Payroll SQL query for Create Table

--- Payroll_Service.sql
+++ Payroll_Service.sql
@@ -1,3 +1,10 @@
 ------------------ UC1 Create Database --------------------------------
 create database Employee_Payroll
 use Employee_Payroll
+----------------------- UC2 Create Table ----------------------
+create table employee_payroll(
+EmployeeId int identity(1,1) primary key,
+EmployeeName varchar(255),
+EmployeeSalary float,
+StartDate Date
+)
